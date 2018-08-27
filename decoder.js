41a2135fa4afff65f046aa888a230655a8d09756
Vincenzo Della Mea @ Medical Informatics & Telemedicine Lab, UNIUD <vincenzo.dellamea@uniud.it>
2018-08-27 14:47:21 +0200

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -1,6 +1,7 @@
 function Decoder(bytes, port) {
-  // Decode an uplink message from two bytes to an object.
+  // Decode an uplink message from two bytes to an object, to be used on TTN with MKRWAN-DHT11 
   //NB DHT-11 temperature range is 0-50, thus no negatives
+  
   var decoded = {};
 
   decoded.humidity = bytes[0];
